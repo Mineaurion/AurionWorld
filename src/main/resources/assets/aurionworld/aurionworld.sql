973b9a7447fcb95f8862f7e340fc3d71f5a79277
Ashk2a <timed2a@hotmail.fr>
2019-09-07 00:06:24 +0200

    [UPDATE] AurionWorld.sql

--- src/main/resources/assets/aurionworld/aurionworld.sql
+++ src/main/resources/assets/aurionworld/aurionworld.sql
@@ -1,20 +1,31 @@
-CREATE TABLE IF NOT EXISTS `worlds`  (
+CREATE TABLE IF NOT EXISTS `worlds` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `owner_uuid` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `path` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `loaded` int(1) NULL DEFAULT NULL,
-  `updated_at` datetime(0) NULL DEFAULT NULL,
-  `created_at` datetime(0) NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `dimension_id` int(11) DEFAULT NULL,
+  `owner_uuid` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `provider` varchar(255) DEFAULT NULL,
+  `seed` varchar(250) DEFAULT NULL,
+  `generator` text,
+  `structures` int(1) DEFAULT NULL,
+  `load_it` int(1) DEFAULT NULL,
+  `spawn_x` int(11) DEFAULT NULL,
+  `spawn_y` int(11) DEFAULT NULL,
+  `spawn_z` int(11) DEFAULT NULL,
+  `spawn_o` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT
 
-CREATE TABLE IF NOT EXISTS `worlds_members`  (
+CREATE TABLE IF NOT EXISTS `worlds_members` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `world_id` int(11) NULL DEFAULT NULL,
-  `member_uuid` int(11) NULL DEFAULT NULL,
-  `updated_at` datetime(0) NULL DEFAULT NULL,
-  `created_at` datetime(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
-
-SET FOREIGN_KEY_CHECKS = 1;
+  `world_id` int(11) DEFAULT NULL,
+  `uuid` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `level` int(1) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `worlds_trusts_FK` (`world_id`),
+  CONSTRAINT `worlds_trusts_FK` FOREIGN KEY (`world_id`) REFERENCES `worlds` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT
